eae6ac256f9311e371899bf57f7952f3eb68af05
Emily <144031038+EmilyVanDenBerg@users.noreply.github.com>
2023-12-24 22:14:54 +0100

    Added scuffed jump feature
    Mouse click to jump (very scuffed, needs bugfixing)

--- walldodger.js
+++ walldodger.js
@@ -6,24 +6,21 @@ canvas.height = 600;
 document.body.appendChild(canvas);
 
 // Plaats de bal in het midden van het scherm, geef een snelheid van 5 en een radius van 15.
-let x = canvas.width / 2;
-let y = canvas.height / 2;
+let x = 400;
+let y = canvas.height / 2; // Start de bal vanuit het midden van het scherm
 let speed = 5;
 let radius = 15;
+let gravity = 1; // Pas de zwaartekrachtwaarde aan om de valssnelheid van de bal te regelen
 
-
-// Variabelen die later worden gebruikt om te kijken of links of rechts word ingedrukt.
-// In de les een andere manier behandeld, maar hierdoor word de interval weggehaald waardoor er minder hapering in de movement zit.
 let leftPressed = false;
 let rightPressed = false;
+let mousePressed = false;
 
-// Process of A of D is ingedrukt/ losgelaten.
+// Event listeners voor toetsenbord en muis
 document.addEventListener('keydown', function(event) {
     if (event.key === 'a' || event.key === 'A') {
         leftPressed = true;
-    } 
-
-    else if (event.key === 'd' || event.key === 'D') {
+    } else if (event.key === 'd' || event.key === 'D') {
         rightPressed = true;
     }
 });
@@ -31,24 +28,73 @@ document.addEventListener('keydown', function(event) {
 document.addEventListener('keyup', function(event) {
     if (event.key === 'a' || event.key === 'A') {
         leftPressed = false;
-    } 
-
-    else if (event.key === 'd' || event.key === 'D') {
+    } else if (event.key === 'd' || event.key === 'D') {
         rightPressed = false;
     }
 });
 
-// Als A wordt ingedrukt (wat volgt uit de vorige lijnen code), maak een linkse beweging.
-// Als D wordt ingedrukt, maak een rechtse beweging.
+document.addEventListener('mousedown', function(event) {
+    if (event.button === 0) {
+        mousePressed = true;
+    }
+});
+
+document.addEventListener('mouseup', function(event) {
+    if (event.button === 0) {
+        mousePressed = false;
+    }
+});
+
+let tellerCyclus = 0;
+let cyclus = 3;
+
+// Functie voor de zwaartekracht
+function ballGravity() {
+    y = y + gravity;
+
+    if (y + radius >= canvas.height) {
+        y = canvas.height - radius; // Als de bal de bodem raakt, stop de val
+        gravity = 1;
+    } else if (y - radius <= 0) {
+        y = radius; // Als de bal het plafond raakt, stop de opwaartse beweging
+        gravity = 1;
+    }
+
+    tellerCyclus++;
+    if (tellerCyclus > cyclus && gravity < 10) {
+        gravity++; // Verhoog de zwaartekracht na een bepaald aantal frames
+        tellerCyclus = 0;
+    }
+    
+    document.addEventListener('mousedown', function(event) {
+        if (event.button === 0) {
+            tellerCyclus++;
+            mousePressed = true;
+            if (tellerCyclus > cyclus) {
+                gravity = gravity - 3;
+                tellerCyclus = 0;
+                if (gravity < -10) {
+                    gravity = gravity + 10;
+                }
+            }
+        }
+    });
+    
+    document.addEventListener('mouseup', function(event) {
+        if (event.button === 0) {
+            mousePressed = false;
+        }
+    });
+}
+
+// Functie om invoer te verwerken
 function handleInput() {
     if (leftPressed) {
         x -= speed;
         if (x - radius < 0) {
             x = radius;
         }
-    } 
-
-    else if (rightPressed) {
+    } else if (rightPressed) {
         x += speed;
         if (x + radius > canvas.width) {
             x = canvas.width - radius;
@@ -56,29 +102,27 @@ function handleInput() {
     }
 }
 
-// Teken het canvas en de bal
+// Functie om het canvas en de bal te tekenen
 function drawCanvasAndBall() {
-    // Bepaal de kleur en vorm van het canvas
-    ctx.fillStyle = "#d9d9d9"; // Grijs als achtergrondkleur
-    ctx.fillRect(0, 0, canvas.width, canvas.height); // Teken een rechthoek om de achtergrond te vullen
+    ctx.fillStyle = "#d9d9d9";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-
-    // Teken de cirkel zelf
     ctx.arc(x, y, radius, 0, Math.PI * 2, true);
     ctx.fillStyle = 'red';
     ctx.fill();
-    ctx.strokeStyle = 'black'; // Zwarte kleur voor de rand
-    ctx.lineWidth = 3; // Dikte van de rand
-    ctx.stroke(); // Teken de rand
-
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 3;
+    ctx.stroke();
     ctx.closePath();
 }
 
-// Constant updates aanroepen elke mogelijke frame
+// Functie voor de constante updates elke mogelijke frame
 function update() {
     drawCanvasAndBall();
     handleInput();
-     requestAnimationFrame(update);
+    ballGravity();
+    requestAnimationFrame(update);
 }
 
+// Start de animatieloop
 update();
